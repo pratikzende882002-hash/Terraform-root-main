fb3223cd125f3ca66eb7de05ddfc623f088da915
Pratik <pratikzende882002@gmail.com>
2026-04-21 12:51:02 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -14,18 +14,18 @@ output "vpc_id" {
 
 output "subnet_ids" {
   description = "All subnet IDs"
-  value       = { for k, v in module.subnets : k => v.subnet_id }
+  value       = { for k, v in module.subnet : k => v.subnet_id }
 }
 
 output "ec2_instance_ids" {
   description = "All EC2 instance IDs"
-  value       = { for k, v in module.ec2_instances : k => v.instance_id }
+  value       = { for k, v in module.ec2_instance : k => v.instance_id }
 }
 
 
 output "ec2_public_ips" {
   description = "Public IPs of EC2 instances"
-  value       = { for k, v in module.ec2_instances : k => v.public_ip }
+  value       = { for k, v in module.ec2_instance : k => v.public_ip }
 }
 
 output "s3_bucket_name" {
